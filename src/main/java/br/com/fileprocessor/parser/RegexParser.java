1efe2ed3cc50f790427864d2152253853725aad9
zardojunior <zardojunior@gmail.com>
2020-07-13 17:16:58 -0300

    Fix exception handling

--- src/main/java/br/com/fileprocessor/parser/RegexParser.java
+++ src/main/java/br/com/fileprocessor/parser/RegexParser.java
@@ -3,14 +3,9 @@ package br.com.fileprocessor.parser;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import br.com.fileprocessor.exception.ParseException;
 
 public class RegexParser {
-
-	private static final Logger log = LoggerFactory.getLogger(RegexParser.class);
 
 	private Pattern pattern;
 
@@ -19,12 +14,12 @@ public class RegexParser {
 	}
 
 	public String[] parse(String string) {
+		Matcher matcher = pattern.matcher(string);
+		if (!matcher.matches()) {
+			String errorMessage = String.format("The string '%s' does not match the regular expression '%s'", string, matcher.pattern());
+			throw new ParseException(errorMessage);
+		}
 		try {
-			Matcher matcher = pattern.matcher(string);
-			if (!matcher.matches()) {
-				log.warn("The sequence '{}' does not match the regular expression '{}'", string, matcher.pattern());
-				return new String[] {};
-			}
 			String[] groups = new String[matcher.groupCount()];
 			for (int i = 0 ; i < matcher.groupCount(); i++) {
 				groups[i] = matcher.group(i+1);
