409521c355fb4b6f159304c259387e74644de873
zardojunior <zardojunior@gmail.com>
2020-07-14 18:52:17 -0300

    Added unit test for FileProcessor

---
+++ src/test/java/br/com/fileprocessor/service/FileProcessorTest.java
@@ -0,0 +1,56 @@
+package br.com.fileprocessor.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileProcessorTest {
+
+    private final String INPUT_FILENAME = "test01.dat";
+    private final String OUTPUT_FILENAME = "test01.done.dat";
+
+    private Path inputFilenamePath;
+    private Path outputFilenamePath;
+
+    @Before
+    public void before() throws IOException {
+        this.inputFilenamePath = getFromResources(INPUT_FILENAME).toPath();
+        this.outputFilenamePath = Paths.get(FileUtils.getTempDirectoryPath(), OUTPUT_FILENAME);
+        Files.deleteIfExists(outputFilenamePath);
+        Assert.assertTrue(isFileAccessible(inputFilenamePath.toFile()));
+        Assert.assertFalse(outputFilenamePath.toFile().exists());
+    }
+
+    @Test
+    public void testProcess() throws IOException {
+        File file = inputFilenamePath.toFile();
+        FileProcessor processor = new FileProcessor(FileUtils.getTempDirectoryPath(), ".done.dat");
+        processor.process(file);
+        Assert.assertTrue(isFileAccessible(outputFilenamePath.toFile()));
+    }
+
+    @After
+    public void after() throws IOException {
+        Files.deleteIfExists(outputFilenamePath);
+    }
+
+    private File getFromResources(String file) {
+        return new File(getClass().getClassLoader().getResource(file).getFile());
+    }
+
+    private boolean isFileAccessible(File file) {
+        return file != null
+                && file.exists()
+                && file.isFile()
+                && file.canRead();
+    }
+
+}
