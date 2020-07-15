5f288f430219e052258b8939ad627526e5f592da
zardojunior <zardojunior@gmail.com>
2020-07-15 03:53:30 -0300

    Added assertion for "ContinueOnUnkownRow"

--- src/test/java/br/com/fileprocessor/config/ConfigurationLoaderTest.java
+++ src/test/java/br/com/fileprocessor/config/ConfigurationLoaderTest.java
@@ -7,12 +7,13 @@ public class ConfigurationLoaderTest {
 
     @Test
     public void testLoad() {
-    	Configuration config = ConfigurationLoader.loadFromResource("config.yml", Configuration.class);
-    	Assert.assertEquals("data/in", config.getInputDir());
-    	Assert.assertEquals("data/out", config.getOutputDir());
-    	Assert.assertEquals(".dat", config.getInputFileExtension());
-    	Assert.assertEquals(".done.dat", config.getOutputFileExtension());
-    	Assert.assertEquals(new Integer(20), config.getPoolingIntervalInSeconds());	
+        Configuration config = ConfigurationLoader.loadFromResource("config.yml", Configuration.class);
+        Assert.assertEquals("data/in", config.getInputDir());
+        Assert.assertEquals("data/out", config.getOutputDir());
+        Assert.assertEquals(".dat", config.getInputFileExtension());
+        Assert.assertEquals(".done.dat", config.getOutputFileExtension());
+        Assert.assertEquals(new Integer(20), config.getPoolingIntervalInSeconds());
+        Assert.assertEquals(true, config.getContinueOnUnkownRow());
     }
 
 }
