a25d890cc1850042162361a06b52e36238f20484
zardojunior <zardojunior@gmail.com>
2020-07-15 02:32:01 -0300

    Get from static map instead of creating a new object everytime

--- src/main/java/br/com/fileprocessor/factory/ConverterFactory.java
+++ src/main/java/br/com/fileprocessor/factory/ConverterFactory.java
@@ -1,9 +1,13 @@
 package br.com.fileprocessor.factory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import br.com.fileprocessor.converter.Converter;
 import br.com.fileprocessor.converter.CustomerConverter;
 import br.com.fileprocessor.converter.SaleConverter;
 import br.com.fileprocessor.converter.SalesmanConverter;
+import br.com.fileprocessor.exception.ConverterNotFoundException;
 import br.com.fileprocessor.model.Customer;
 import br.com.fileprocessor.model.Model;
 import br.com.fileprocessor.model.Sale;
@@ -15,16 +19,23 @@ public class ConverterFactory {
     public static final String CUSTOMER_TYPE = Customer.TYPE;
     public static final String SALESMAN_TYPE = Salesman.TYPE;
 
+    private static Map<String, Converter<String[], Model>> converters = new HashMap<>();
+    static {
+        converters.put(SALE_TYPE, new SaleConverter());
+        converters.put(CUSTOMER_TYPE, new CustomerConverter());
+        converters.put(SALESMAN_TYPE, new SalesmanConverter());
+    }
+
     public static Converter<String[], Model> create(String type) {
-        switch (type) {
-        case SALE_TYPE:
-            return new SaleConverter();
-        case CUSTOMER_TYPE:
-            return new CustomerConverter();
-        case SALESMAN_TYPE:
-            return new SalesmanConverter();
-        default:
-            return null;
+        return converters.get(type);
+    }
+
+    public static Converter<String[], Model> createThrowsException(String type) {
+        Converter<String[], Model> converter = create(type);
+        if (converter == null) {
+            String errorMessage = String.format("No converter found. Valid types are %s", converters.keySet());
+            throw new ConverterNotFoundException(errorMessage);
         }
+        return converter;
     }
 }
--- src/main/java/br/com/fileprocessor/factory/ParserFactory.java
+++ src/main/java/br/com/fileprocessor/factory/ParserFactory.java
@@ -1,5 +1,9 @@
 package br.com.fileprocessor.factory;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.fileprocessor.exception.ParserNotFoundException;
 import br.com.fileprocessor.model.Customer;
 import br.com.fileprocessor.model.Sale;
 import br.com.fileprocessor.model.Salesman;
@@ -14,16 +18,23 @@ public class ParserFactory {
     public static final String CUSTOMER_TYPE = Customer.TYPE;
     public static final String SALESMAN_TYPE = Salesman.TYPE;
 
+    private static Map<String, Parser<String, String[]>> parsers = new HashMap<>();
+    static {
+        parsers.put(SALE_TYPE, new SaleParser());
+        parsers.put(CUSTOMER_TYPE, new CustomerParser());
+        parsers.put(SALESMAN_TYPE, new SalesmanParser());
+    }
+
     public static Parser<String, String[]> create(String type) {
-        switch (type) {
-        case SALE_TYPE:
-            return new SaleParser();
-        case CUSTOMER_TYPE:
-            return new CustomerParser();
-        case SALESMAN_TYPE:
-            return new SalesmanParser();
-        default:
-            return null;
+        return parsers.get(type);
+    }
+
+    public static Parser<String, String[]> createThrowsException(String type) {
+        Parser<String, String[]> parser = create(type);
+        if (parser == null) {
+            String errorMessage = String.format("No parser found. Valid types are %s", parsers.keySet());
+            throw new ParserNotFoundException(errorMessage);
         }
+        return parser;
     }
 }
